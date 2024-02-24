2b3adee5f83b08fa099d01ad26641da9fe3e96d5
Alphocado <raylienardy@gmail.com>
2024-02-24 12:15:17 +0800

    object create

--- Web/JavaScript/Advance/01_object/app.js
+++ Web/JavaScript/Advance/01_object/app.js
@@ -1,5 +1,6 @@
 // cara membuat object pada javascript
 // 1. object literal
+// problem : tidak efektif kalau object lebih dari satu
 // let mahasiswa1 = {
 //   nama: 'Udin',
 //   energy: 10,
@@ -19,6 +20,7 @@
 // note: method adalah function yang ada didalam object
 
 // 2. function declaration
+// problem : berat
 // function Mahasiswa(nama, energy){
 //   let mahasiswa = {};
 //   mahasiswa.nama = nama;
@@ -42,22 +44,48 @@
 // 3. constructor function
 // ganti mahasiswa dengan this
 // hapus let mahasiswa & hapus return
-function Mahasiswa(nama, energy){
-  this.nama = nama;
-  this.energy = energy;
+// function Mahasiswa(nama, energy){
+//   this.nama = nama;
+//   this.energy = energy;
 
-  this.makan = function (porsi) {
-    this.energy += porsi;
-    console.log(`selamat makan ${this.nama}`);
-  }
+//   this.makan = function (porsi) {
+//     this.energy += porsi;
+//     console.log(`selamat makan ${this.nama}`);
+//   }
 
-  this.main = function(jam) {
-    this.energy -= jam;
-    console.log(`${this.nama} sedang main`);
-  }
-}
+//   this.main = function(jam) {
+//     this.energy -= jam;
+//     console.log(`${this.nama} sedang main`);
+//   }
+// }
 
-let udin = new Mahasiswa('udin', 10);
+// let udin = new Mahasiswa('udin', 10);
 
 
 // 4. object.create
+
+const methodMahasiswa = {
+  makan: function (porsi) {
+    this.energy += porsi;
+    console.log(`selamat makan ${this.nama}`);
+  },
+
+  main: function(jam) {
+    this.energy -= jam;
+    console.log(`${this.nama} sedang main`);
+  },
+
+  tidur: function(jam) {
+    this.energy += jam*2;
+    console.log(`tidur ${this.nama}`)
+  }
+};
+function Mahasiswa(nama, energy){
+  let mahasiswa = Object.create(methodMahasiswa);
+  mahasiswa.nama = nama;
+  mahasiswa.energy = energy;
+
+  return mahasiswa;
+}
+let udin = Mahasiswa('udin', 10);
+let tono = Mahasiswa('tono', 10);
