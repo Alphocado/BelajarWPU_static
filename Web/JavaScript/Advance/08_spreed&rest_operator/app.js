54badf83a4a003e53798066b742f3a54f62777da
Ray <91828843+Alphocado@users.noreply.github.com>
2024-02-26 12:49:09 +0800

    rest parameter

--- Web/JavaScript/Advance/08_spreed&rest_operator/app.js
+++ Web/JavaScript/Advance/08_spreed&rest_operator/app.js
@@ -38,6 +38,47 @@
 // console.log(mhs);
 
 
-const nama = document.querySelector('.nama');
-const huruf = [...nama.textContent].map(h => `<span>${h}</span>`).join('');
-nama.innerHTML=huruf;
+// const nama = document.querySelector('.nama');
+// const huruf = [...nama.textContent].map(h => `<span>${h}</span>`).join('');
+// nama.innerHTML=huruf;
+
+// function myFunc(..k){
+//   // return Array.from(arguments);
+//   return [];
+// }
+// console.log(myFunc(1,2,3,4,5));
+
+// function jumlahkan(...angka){
+//   let total = 0;
+//   for(const a of angka) {
+//     total += a;
+//   }
+//   return total;
+// }
+// console.log(jumlahkan(1,2,3,4,5));
+
+
+// array destructuring
+// const klmp1 = ['udin', 'budi', 'tono',' anji'];
+// const [ketua, wakil, ...anggota] = klmp1;
+// console.log()
+
+// object destructuring
+// const team = {
+//   pm: "udin",
+//   frontEnd1: 'budi',
+//   frontEnd2: 'erik',
+//   backEnd: 'tono',
+//   ux: 'fani',
+//   devOps: 'anji'
+// };
+
+// const {pm, ...myTeam} = team;
+// console.log(myTeam);
+
+// filtering
+function filterBy(type, ...values){
+  return values.filter( v => typeof v === type );
+}
+
+console.log(filterBy('boolean', 1,2, 'udin', false, 10, true, 'tono'));
