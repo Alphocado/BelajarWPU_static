a434d557eccb52475d087c45231bfffc4ceb05e1
Ray <91828843+Alphocado@users.noreply.github.com>
2024-02-26 10:06:49 +0800

    desctructuring function

--- Web/JavaScript/Advance/06_destructuring/app.js
+++ Web/JavaScript/Advance/06_destructuring/app.js
@@ -102,9 +102,9 @@
 // console.log(getIdMhs(mhs));
 
 // Destructuring
-function calc(a,b) {
-  return [a+b, a-b, a/b, a*b];
-}
+// function calc(a,b) {
+//   return [a+b, a-b, a*b, a / b];
+// }
 // const jumlah = penjumlahanPerkalian(2,3)[0];
 // const kali = penjumlahanPerkalian(2,3)[1];
 
@@ -112,5 +112,38 @@ function calc(a,b) {
 // console.log(jumlah);
 // console.log(kali);
 
-const [tambah, kurang, bagi, kali] = calc(2,3);
-console.log(bagi);
+// const [tambah, kurang, kali, bagi = 'tidak ada'] = calc(2,3);
+// console.log(bagi);
+
+// dgn object bisa inisiasi tanpa berurut
+// function kalkulasi(a,b) {
+//   return {
+//     tambah: a+b,
+//     kurang: a-b,
+//     kali: a*b,
+//     bagi: a/b,
+//   }
+// }
+// const {bagi, tambah, kali, kurang} = kalkulasi(2,3);
+// console.log(kurang);
+
+// Destructuring function arguments
+const mhs1 = {
+  nama: 'udin',
+  umur: 33,
+  email: 'udin@gmail.com',
+  nilai: {
+    tugas: 80,
+    uts: 85,
+    uas: 70
+  }
+};
+// function cetakMhs(mhs){
+//   return `Halo, im ${mhs.nama}, im ${mhs.umur} years old`
+// }
+// console.log(cetakMhs(mhs1));
+
+function cetakMhs({nama, umur, nilai: {tugas, uts, uas}}){
+  return `Halo, saya ${nama}, saya ${umur}, nilai ${nilai.uas}`;
+};
+console.log(cetakMhs(mhs1));
