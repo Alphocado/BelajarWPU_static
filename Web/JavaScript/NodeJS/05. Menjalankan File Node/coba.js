50e87979b5bf513f81a09dc3064fb5de8fafc3df
Ray <91828843+Alphocado@users.noreply.github.com>
2024-03-12 13:40:32 +0800

    5. Menjalankan File Node

---
+++ Web/JavaScript/NodeJS/05. Menjalankan File Node/coba.js
@@ -0,0 +1,12 @@
+// const nama = (nama) =>  `hai ${nama}`;
+// console.log(nama('udin'));
+
+
+// node tidak mengenal window
+// console.log(window);
+
+// import dari file lain
+// require('./index');
+const cetakNama = require('./index');
+
+console.log(cetakNama('udin'))
---
+++ Web/JavaScript/NodeJS/05. Menjalankan File Node/index.js
@@ -0,0 +1,5 @@
+function cetakNama(nama){
+  return `udin ${nama}`;
+} 
+
+module.exports = cetakNama;
